f37a228bd0eecaf8e52d8d2f6ec13e18d0e249d8
DWDW2 <daid8002@gmail.com>
2024-06-05 22:25:45 +0500

    some other commits

--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 import React from 'react'
 const Navbar = () => {
   return (
-        <div class="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
-            <h1 class="text-lg font-semibold">Nfac - blog</h1>
-            <ul class="flex gap-[40px] text-m">
+        <div className="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
+            <h1 className="text-lg font-semibold">Nfac - blog</h1>
+            <ul className="flex gap-[40px] text-m">
                 <li><Link href='/'>Home</Link></li>
                 <li><Link href='/Posts/main'>Posts</Link></li>
                 <li><Link href='/Blogs'>blogs</Link></li>
