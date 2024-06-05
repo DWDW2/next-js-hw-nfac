1a08abeefe3689bd13603cde6a4a9d53510669de
DWDW2 <daid8002@gmail.com>
2024-06-05 21:02:32 +0500

    basic-front

---
+++ app/components/layout/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+const Navbar = () => {
+  return (
+        <div class="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
+            <h1 class="text-lg font-semibold">Nfac - blog</h1>
+            <ul class="flex gap-[40px] text-m">
+                <li><Link href='/'>Home</Link></li>
+                <li><Link href='/Posts/main'>Posts</Link></li>
+                <li><Link href='/Blogs'>blogs</Link></li>
+                <li><Link href='/About'>About</Link></li>
+            </ul>
+        </div>
+  )
+}
+
+export default Navbar
---
+++ app/components/layout/Post_inner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Post_inner = (props) => {
+    
+  return (
+    <div className='text-center mt-10 bg-blue-400 w-3/4 mx-auto my-auto p-10'>
+            <h1 className='text-[25px] text-bold'>
+                {props.title}
+            </h1>
+        <div className='grid grid-cols-1 gap-2 w-60 mx-auto my-auto sm:grid-cols-2 '>
+            <span className='text-[16px]'>
+                {props.author}
+            </span>
+            <span className='text-[16px]'>
+                {props.date}
+            </span>
+        </div>
+        <div className='mt-10'>
+            {props.description}
+        </div>
+    </div>
+  )
+}
+
+export default Post_inner
